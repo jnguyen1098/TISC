6d40123b64b6da8d270877c766587ce2008d776c
jnguyen1098 <jnguyen1098@gmail.com>
2021-03-26 13:50:39 -0400

    test suite

--- test.sh
+++ test.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-echo "hello"
+SRC_DIR="test/src"
+TEST_DIR="test/asm"
+IN_DIR="test/in"
+OUT_DIR="test/out"
+
+BIN=./bin/TISC
+
+TESTS="gcd fac sort"
+
+make all
+
+for test in $TESTS; do
+    echo "Testing $test.tm"
+    "$BIN" "$TEST_DIR"/"$test".tm < "$IN_DIR"/"$test".in > "$test_output.tmp"
+    diff "$test_output.tmp" "$OUT_DIR"/"$test".out
+    rm -rf "$test_output.tmp"
+done
+
+echo "Done tests"
