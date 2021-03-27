7dde618365d098d02c721e4f3fc83025530d6242
jnguyen1098 <jnguyen1098@gmail.com>
2021-03-26 23:09:56 -0400

    update gitignore to exclude test files (these can be regenerated whenever), remove other fflush(stdin) call, delete the bak file in src, as it has already been moved to test/

--- src/TISC.c
+++ src/TISC.c
@@ -430,7 +430,6 @@ int doCommand (void)
     do
     { 
         printf ("Enter command: ");
-        fflush (stdin);
         fflush (stdout);
         fgets(in_Line, LINESIZE - 2, stdin);
         lineLen = strlen(in_Line);
